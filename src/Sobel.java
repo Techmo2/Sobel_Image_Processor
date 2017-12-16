d50c8aa4354a28aa398ceec6b2dd15834112f837
Bradly Tillmann <bradly@orbisnetwork.net>
2017-12-15 18:59:30 -0700

    Made a more comprehensive demonstration

--- src/Sobel.java
+++ src/Sobel.java
@@ -1,24 +1,47 @@
 
 public class Sobel {
+	private static final int Combined = 1;
+	private static final int SmallKernel = 2;
+	private static final int LargeKernel = 3;
+	
+	private static final int demonstrationType = Combined;
+	
 	public static void main(String[] args) {
 		ImageProcessor ip = new ImageProcessor();
-		Kernel sobel = new SobelKernel7(SobelMode.Approximate);
-		RawImage image = new RawImage("test.jpg");
-		int[][] processed = ip.ProcessImage(sobel, image, 1); // 5 iterations
-		ip.writeImage(processed, "out.jpg");
-		ip.writeImage(image.rawImageData(), "pre.jpg");
+		Kernel sobel7 = new SobelKernel7(KernelMode.Exact);
+		Kernel sobel3 = new SobelKernel3(KernelMode.Exact);
 		
-		System.out.println("Average image value pre processing: " + image.averageValue());
+		long startTime = System.currentTimeMillis();
+		RawImage image = new RawImage("test.jpg"); // Load the base image file
+		float runTime = (System.currentTimeMillis() - startTime) / 1000.0f;
+		System.out.println("Image loaded in " + runTime + " seconds.");
 		
-		int[][] imageData = processed;
-		int average = 0;
-		for(int x = 0; x < imageData.length; x++) {
-			for(int y = 0; y < imageData[0].length; y++) {
-				average += imageData[x][y];
-			}
+		startTime = System.currentTimeMillis();
+		int[][] processed;
+		
+		if(demonstrationType == Combined) {
+			System.out.println("Running combined kernels.");
+			processed = ip.ProcessImage(sobel3, image, 1); // Process with 3x3
+			processed = ip.ProcessImage(sobel7, new RawImage(processed), 1); // Then process with 7x7
+			processed = ip.amplifyImage(processed, 2.0f);
 		}
-		average /= (imageData.length * imageData[0].length);
+		else if(demonstrationType == SmallKernel) {
+			System.out.println("Running small kernel.");
+			processed = ip.ProcessImage(sobel3, image, 5); // Process with 3x3, 5 times
+			processed = ip.amplifyImage(processed, 1.5f); // The image gets a little dark
+		}
+		else if(demonstrationType == LargeKernel) {
+			System.out.println("Running large kernel.");
+			processed = ip.ProcessImage(sobel7, image, 1); // Then process with 7x7
+		}
+		else {
+			System.out.println("I don't know what you want me to do.");
+			System.exit(-1);
+		}
 		
-		System.out.println("Average image value post processing: " + average);
+		runTime = (System.currentTimeMillis() - startTime) / 1000.0f;
+		System.out.println("Sobel finished in " + runTime + " seconds.");
+		ip.writeImage(processed, "out.jpg"); // The final processed image
+		ip.writeImage(image.rawImageData(), "pre.jpg"); // The image after it is loaded, but before it's processed
 	}
 }
